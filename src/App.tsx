262cf6d536feef960c0a71f9a34ba3f6910f99be
Mateo Rodriguez <mdavidrodriguez@unicesar.edu.co>
2024-03-22 13:06:59 -0500

    App calculadora de Propinas y consumos

---
+++ src/App.tsx
@@ -0,0 +1,37 @@
+import MenuItem from "./components/MenuItem";
+import OrderContents from "./components/OrderContents";
+import { menuItems } from "./data/db";
+import useOrder from "./hooks/useOrder";
+import OrderTotals from "./components/OrderTotals";
+import TipPercentajeForm from "./components/TipPercentajeForm";
+
+function App() {
+  const { addItem, order, removeItem, tip, setTip } = useOrder();
+  return (
+    <>
+      <header className="bg-teal-400 py-5">
+        <h1 className="text-center text-4xl font-black">
+          Calculadora de Propinas y Consumo
+        </h1>
+      </header>
+      <main className="max-w-7xl mx-auto py-20 grid md:grid-cols-2 ">
+        <div className="p-5">
+          <h2 className="text-4xl font-black">Menú</h2>
+          <div className="space-y-3 mt-10">
+            {menuItems.map((item) => (
+              <MenuItem key={item.id} item={item} addItem={addItem} />
+            ))}
+          </div>
+        </div>
+
+        <div className="border border-dashed border-slate-300 p-5 rounded-lg space-y-10">
+          <OrderContents order={order} removeItem={removeItem} />
+          <TipPercentajeForm setTip={setTip} />
+          <OrderTotals order={order} tip={tip} />
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/OrderTotals.tsx
@@ -0,0 +1,40 @@
+import { useMemo } from "react";
+import { OrderItem } from "../types";
+import { formatCurrency } from "../helpers";
+
+type OrderTotalsProps = {
+  order: OrderItem[];
+  tip: number;
+};
+export default function OrderTotals({ order, tip }: OrderTotalsProps) {
+  const subtotalAmount = useMemo(
+    () => order.reduce((total, item) => total + item.quantity * item.price, 0),
+    [order]
+  );
+  const tipAmount = useMemo(() => subtotalAmount * tip, [tip, subtotalAmount]);
+  const totalAmount = useMemo(
+    () => subtotalAmount + tipAmount,
+    [subtotalAmount, tipAmount]
+  );
+
+  return (
+    <>
+      <div className="space-y-3">
+        <h2 className="font-black text-2xl ">Totales y Propinas</h2>
+        <p>
+          Subtotal a pagar: {""}
+          <span className="font-bold">{formatCurrency(subtotalAmount)}</span>
+        </p>
+        <p>
+          Propina: {""}
+          <span className="font-bold">{formatCurrency(tipAmount)}</span>
+        </p>
+        <p>
+          Total a Pagar: {""}
+          <span className="font-bold">{formatCurrency(totalAmount)}</span>
+        </p>
+      </div>
+      <button></button>
+    </>
+  );
+}
